1daebe4c7a43679cf7d9ecf95573c6cb433ca715
Alibek Omarov <a1ba.omarov@gmail.com>
2023-05-03 04:30:05 +0300

    VGui2Interfaces: remove Xash3D FWGS detection

--- include/VGui2Interfaces.h
+++ include/VGui2Interfaces.h
@@ -25,12 +25,7 @@ from your version.
 #ifndef VGUI2INTERFACES_H
 #define VGUI2INTERFACES_H
 
-// HACK: Detect Xash3D FWGS
-#ifdef XASH_BUILD_COMMIT
 #include "VFileSystem009.h"
-#else
-#include "FileSystem.h"
-#endif
 #include "IInput.h"
 #include "IInputInternal.h"
 #include "IKeyValues.h"
@@ -46,7 +41,6 @@ IKeyValues *keyvalues();
 
 namespace vgui2
 {
-
 IVGui *ivgui();
 ILocalize *localize();
 IFileSystem *filesystem();
@@ -59,7 +53,6 @@ IInputInternal *inputinternal();
 
 bool InitializeVGui2Interfaces( const char *moduleName, CreateInterfaceFn *factories, int factoryCount );
 const char *GetModuleName();
-
-}; // namespace vgui2
+} // namespace vgui2
 
 #endif // VGUI2INTERFACES_H
